3dc86cc4ab45e8d19f91a20475f4f5770190ef37
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2024-04-25 13:19:31 +0200

    feat: add footer to the page

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,9 +1,60 @@
 import '../style/Footer.css';
 
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+
+import Logo from '../assets/img/Combined-Shape.png'
+
 function Footer() {
     return(
         <footer className='footer'>
-
+            <div className='centBlockFo'>
+                <section>
+                    <div className='logoPages'>
+                        <img src={Logo} alt="Logo" />
+                        <h3>Photographers & videographers capturing the world around us.</h3>
+                    </div>
+                    <div className='tablePages'>
+                        <article>
+                            <h1>Business areas</h1>
+                            <h2>Product Photography</h2>
+                            <h2>Architecture Photography</h2>
+                            <h2>Drone Photography</h2>
+                            <h2>Wildlife Photography</h2>
+                        </article>
+                        <article>
+                            <h1>Pages</h1>
+                            <h2>Gear cage</h2>
+                            <h2>Featured images</h2>
+                            <h2>Contact</h2>
+                            <h2>Style guide</h2>
+                            <h2>Instructions</h2>
+                            <h2>Changelog</h2>
+                        </article>
+                    </div>
+                </section>
+                <div className='line'></div>
+                <section className='textArea'>
+                    <article>
+                        <h4>Subscribe to our newsletter</h4>
+                        <h5>Read about all the things we do.</h5>
+                    </article>
+                    <div className='textBlockBut'>
+                        <textarea name="link" id="link" cols="" rows=""></textarea>
+                        <button></button>
+                    </div>
+                </section>
+                <div className='line'></div>
+                <section className='linkSect'>
+                    <h6>© Aperture Photography, Inc. All rights reserved. Licensing</h6>
+                    <article className='blockLinks'>
+                        <FaXTwitter />
+                        <FaInstagram />
+                        <FaFacebookF />
+                    </article>
+                </section>
+            </div>
         </footer>
     )
 }
